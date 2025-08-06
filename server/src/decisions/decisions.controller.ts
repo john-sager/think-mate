c1219b41ec9e54db3ba058d4030e5ae5a6547852
john.sager <jsagertx@gmail.com>
2025-08-06 03:55:25 -0500

    added type-orm, initial e2e transactions

--- server/src/decisions/decisions.controller.ts
+++ server/src/decisions/decisions.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DecisionsService } from './decisions.service';
+
+class CreateDecisionDto {
+  title: string;
+}
 
 @Controller('decisions')
 export class DecisionsController {
   constructor(private decisionsService: DecisionsService) {}
 
   @Get()
-  getAllDecisions(): Decision[] {
-    return this.decisionsService.getAllDecisions();
+  async findAll(): Promise<Decision[]> {
+    return await this.decisionsService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createDecDto: CreateDecisionDto): Promise<void> {
+    await this.decisionsService.create(createDecDto.title);
   }
 }
