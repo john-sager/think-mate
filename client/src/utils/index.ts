089ad6c105ae3313f6173b6a8a9feb8e81de04eb
john.sager <jsagertx@gmail.com>
2025-12-07 16:04:03 -0600

    initial factor layout

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -3,3 +3,17 @@ export function omitId<T extends { id: string }>(obj: T): Omit<T, "id"> {
   void _id; // mark as intentionally unused
   return rest;
 }
+
+/**
+ * filters a list on a provided predicate into seperate filtered and unfiltered lists.
+ * returns a tuple of filtered an non-filterd arrays.
+ */
+export const filterfy = <T>(items: T[], predicate: (item: T) => boolean) => {
+  return items.reduce(
+    (res: [T[], T[]], item) => {
+      res[predicate(item) ? 0 : 1].push(item);
+      return res;
+    },
+    [[], []]
+  );
+};
