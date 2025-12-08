a6315464d8edf13ec2d02600bd3084c574f0ef94
john.sager <jsagertx@gmail.com>
2025-12-08 02:52:56 -0600

    invalidate decisions query on create

--- client/src/modules/DecisionDiscovery/decisionDiscovery.hooks.ts
+++ client/src/modules/DecisionDiscovery/decisionDiscovery.hooks.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useQueryDecisions = () => {
   const { data, ...rest } = useQuery<Decision[]>({
@@ -9,6 +9,8 @@ export const useQueryDecisions = () => {
 };
 
 export const useCreateDecision = () => {
+  const queryClient = useQueryClient();
+
   const { mutate, ...mutation } = useMutation({
     mutationFn: (newDecision: CreateDecisionDto) => {
       return fetch(`http://localhost:3000/decisions`, {
@@ -16,6 +18,9 @@ export const useCreateDecision = () => {
         body: JSON.stringify(newDecision),
         headers: new Headers({ "Content-Type": "application/json" }),
       });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["decisions"] });
     },
   });
 
